aefd892b5e1810e896e0cde699e0e4cd4cee11f8
gandalfr-KY <suraimuempera@gmail.com>
2023-06-20 18:24:26 +0900

    fixed permission

--- benchmarks/Kokkos/build.sh
+++ benchmarks/Kokkos/build.sh
@@ -2,18 +2,18 @@
 
 cd $(dirname $0)
 docker build -t my_kokkos_image .
-docker run --rm -u 0 -ti --mount type=bind,source="$(pwd)",target=/app my_kokkos_image \
+docker run --rm -u $(id -u):$(id -g) -ti --mount type=bind,source="$(pwd)",target=/app my_kokkos_image \
     bash -c "\
         if [ ! -d /app/include/kokkos ]; then \
             git clone --recursive https://github.com/kokkos/kokkos.git /app/include/kokkos; \
         fi && \
         mkdir -p /app/include/kokkos/build && \
         cd /app/include/kokkos/build && \
-        cmake .. && \
+        cmake .. -DCMAKE_INSTALL_PREFIX=/app/install && \
         make install && \
         
         mkdir -p /app/build && \
         cd /app/build && \
-        cmake .. && \
+        cmake .. -DCMAKE_PREFIX_PATH=/app/install && \
         make && \
         cp myTarget ../main.out"
