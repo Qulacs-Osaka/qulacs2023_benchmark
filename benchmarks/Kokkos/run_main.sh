bc7fad9cfa3146915a4af83be5b35e89561bc1c4
gandalfr-KY <90887153+gandalfr-KY@users.noreply.github.com>
2023-06-20 16:32:31 +0900

    Update run_main.sh

--- benchmarks/Kokkos/run_main.sh
+++ benchmarks/Kokkos/run_main.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 cd $(dirname $0)
-docker run --rm --mount type=bind,source="$(pwd)",target=/app kokkos /app/main.out
+docker run --rm --mount type=bind,source="$(pwd)",target=/app quay.io/cbgeo/kokkos /app/main.out
