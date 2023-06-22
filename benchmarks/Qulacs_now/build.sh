dd5f68f40299eb3c6c89af6c5a5bddca98cb118e
Glacialte <pikutaro25@gmail.com>
2023-06-22 18:50:19 +0900

    update main.cu, build.sh, build_by_docker.sh

--- benchmarks/Qulacs_now/build.sh
+++ benchmarks/Qulacs_now/build.sh
@@ -2,7 +2,5 @@
 
 cd $(dirname $0)
 
-apt-get update
-
 docker build -t qulacs_now .
-docker run -it qulacs_now
+docker run -rm -it -v ./:home/ qulacs_now
--- benchmarks/Qulacs_now/build_by_docker.sh
+++ benchmarks/Qulacs_now/build_by_docker.sh
@@ -8,5 +8,5 @@ if [ ! -d qulacs ]; then
     sh qulacs/script/build_gcc_with_gpu.sh
 fi
 
-nvcc -O2 -I ./qulacs/include -L ./qulacs/lib main.cu -lvqcsim_static -lcppsim_static -lcsim_static -lgpusim_static -D _USE_GPU -lcublas -Xcompiler -fopenmp -o main
+nvcc -O2 -I ./qulacs/include -L ./qulacs/lib main.cu -lvqcsim_static -lcppsim_static -lcsim_static -lgpusim_static -D _USE_GPU -lcublas -Xcompiler -fopenmp -o home/main
 
