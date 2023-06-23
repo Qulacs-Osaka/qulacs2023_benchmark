ea2099300432a5969aac815a2d0d0f8d4f0d10a9
ikanago <ikanago-dev@protonmail.com>
2023-06-23 16:21:54 +0900

    Run docker from runner script

--- qulacs2023_benchmark/run.py
+++ qulacs2023_benchmark/run.py
@@ -13,6 +13,26 @@ class BenchmarkCase:
     n_qubits_end: int
     n_repeat: int
 
+    def build_image(self) -> None:
+        subprocess.run(["docker", "build", "-t", f"qulacs2023_benchmarks/{self.directory}:latest", f"benchmarks/{self.directory}/"])
+
+    def run_benchmark(self) -> BenchmarkResult:
+        means = []
+        for n_qubits in range(self.n_qubits_begin, self.n_qubits_end + 1):
+            mount_config = f"type=bind,source=$(pwd)/benchmark/{self.directory},target=/benchmark"
+            image_tag = f"qulacs2023_benchmarks/{self.directory}:latest"
+            subprocess.run(["docker", "run", "--rm", "-it", "--gpus", "all", "--mount", mount_config, image_tag, "/benchmark/build.sh"], capture_output=True)
+            output = subprocess.run(["docker", "run", "--rm", "-it", "--gpus", "all", "--mount", mount_config, image_tag, "/benchmark/main"], capture_output=True)
+            mean = calculate_mean(output.stdout)
+            means.append(mean)
+        return BenchmarkResult(self.directory, self.n_qubits_begin, self.n_qubits_end, self.n_repeat, means)
+
+    def calculate_mean(output: bytes) -> float:
+        """Calculate mean of time from output of subprocess.run()"""
+        output_str = output.decode("utf-8").strip()
+        durations = list(map(float, output_str.split()))
+        return statistics.fmean(durations)
+
 
 @dataclass
 class BenchmarkResult:
@@ -25,34 +45,17 @@ class BenchmarkResult:
     def save(self, output_directory: str):
         with open(f"{output_directory}/{self.directory}.json", "w") as f:
             d = asdict(self)
-            json.dump(d, f)
-
-
-def calculate_mean(output: bytes) -> float:
-    """Calculate mean of time from output of subprocess.run()"""
-    output_str = output.decode("utf-8").strip()
-    durations = list(map(float, output_str.split()))
-    return statistics.fmean(durations)
-
-
-def run_benchmark(case: BenchmarkCase):
-    means = []
-    for n_qubits in range(case.n_qubits_begin, case.n_qubits_end + 1):
-        subprocess.run([f"./benchmark/{case.directory}/build.sh"], stdout=subprocess.DEVNULL)
-        output = subprocess.run([f"./benchmark/{case.directory}/benchmark", str(n_qubits), str(case.n_repeat)], capture_output=True)
-        mean = calculate_mean(output.stdout)
-        means.append(mean)
-    return BenchmarkResult(case.directory, case.n_qubits_begin, case.n_qubits_end, case.n_repeat, means)
+            json.dump(d, f, indent=4)
 
 
 @click.command
 @click.option("--directory", "-d", default=["qulacs"], multiple=True)
-@click.option("--n_qubits_begin", "-b", default=2)
-@click.option("--n_qubits_end", "-e", default=25)
-@click.option("--n_repeat", "-r", default=10)
+@click.option("--n_qubits_begin", "-b", default=2, type=int, help="Number of qubits to start benchmark")
+@click.option("--n_qubits_end", "-e", default=25, type=int, help="Number of qubits to end benchmark, inclusive")
+@click.option("--n_repeat", "-r", default=10, type=int, help="Number of times to repeat benchmark")
 def main(directory: list[str], n_qubits_begin: int, n_qubits_end: int, n_repeat: int):
     cases = [BenchmarkCase(d, n_qubits_begin, n_qubits_end, n_repeat) for d in directory]
-    results = [run_benchmark(case) for case in cases]
+    results = [case.run_benchmark() for case in cases]
     for result in results:
         result.save("results")
 
