6e83d3bd728f38408661fd368bf27abefae78712
herring101 <herring101@outlook.jp>
2023-07-06 15:46:59 +0900

    x,y,z,h gate

--- benchmarks/openacc/main.cpp
+++ benchmarks/openacc/main.cpp
@@ -1,17 +1,44 @@
 #include <complex>
 #include <iostream>
 #include <vector>
+#include <chrono>
+#include <fstream>
 
-using UINT = unsigned int;
-using ITYPE = unsigned long long;
-using CTYPE = std::complex<double>;
+using UINT = uint64_t;
+using Complex = std::complex<double>;
 
-void update_with_x(std::vector<CTYPE> &state, int n, int target)
+void update_with_x(Complex &a0, Complex &a1)
+{
+    std::swap(a0, a1);
+}
+
+void update_with_y(Complex &a0, Complex &a1)
+{
+    Complex i(0, 1);
+    std::swap(a0, a1);
+    a0 *= -i;
+    a1 *= i;
+}
+
+void update_with_z(Complex &a0, Complex &a1)
+{
+    a1 *= -1;
+}
+
+void update_with_h(Complex &a0, Complex &a1)
+{
+    Complex a0_old = a0;
+    Complex a1_old = a1;
+    a0 = (a0_old + a1_old) / sqrt(2);
+    a1 = (a0_old - a1_old) / sqrt(2);
+}
+
+void update_with_single_control_single_target_gate(std::vector<Complex> &state, int n, int control, int target, void (*gate)(Complex &, Complex &))
 {
     int range1 = 1 << (n - target - 1);
     int range2 = 1 << target;
 
-    CTYPE *state_ptr = state.data();
+    Complex *state_ptr = state.data();
 #pragma acc data copy(state_ptr[0 : 1 << n])
     {
 #pragma acc parallel loop
@@ -21,27 +48,52 @@ void update_with_x(std::vector<CTYPE> &state, int n, int target)
             {
                 int i = (idx1 << (target + 1)) | idx2;
                 int j = i | (1 << target);
-                CTYPE tmp = state_ptr[i];
-                state_ptr[i] = state_ptr[j];
-                state_ptr[j] = tmp;
+                gate(state_ptr[i], state_ptr[j]);
             }
         }
     }
 }
 
-int main()
+int main(int argc, char **argv)
 {
-    int n = 4;
-    std::cout << n << std::endl;
+    if (argc < 3)
+    {
+        std::cerr << "Usage: " << argv[0] << " <n_qubits> <n_repeats>" << std::endl;
+        return 1;
+    }
+    const auto n_qubits = std::strtoul(argv[1], nullptr, 10);
+    const auto n_repeats = std::strtoul(argv[2], nullptr, 10);
+    std::vector<unsigned long long> execution_time(n_repeats);
 
-    std::vector<CTYPE> init_state(1 << n);
+    for (int repeat_itr = 0; repeat_itr < n_repeats; repeat_itr++)
+    {
+        auto st_time = std::chrono::system_clock::now();
 
-    for (int i = 0; i < (1 << n); i++)
-        init_state[i] = i;
+        std::vector<Complex> state(1ULL << n_qubits, 0);
+        for (int i = 0; i < 1 << n_qubits; i++)
+            state[i] = i;
 
-    update_with_x(init_state, n, 1);
+        update_with_single_control_single_target_gate(state, n_qubits, 0, 1, update_with_x);
+        update_with_single_control_single_target_gate(state, n_qubits, 0, 2, update_with_x);
+        update_with_single_control_single_target_gate(state, n_qubits, 0, 3, update_with_x);
+        update_with_single_control_single_target_gate(state, n_qubits, 0, 4, update_with_x);
 
-    for (int i = 0; i < (1 << n); i++)
-        std::cout << ' ' << init_state[i];
+        auto ed_time = std::chrono::system_clock::now();
+        execution_time[repeat_itr] = std::chrono::duration_cast<std::chrono::nanoseconds>(ed_time - st_time).count();
+    }
+
+    std::ofstream ofs("durations.txt");
+    if (!ofs.is_open())
+    {
+        std::cerr << "Failed to open file" << std::endl;
+        return 1;
+    }
+
+    for (int i = 0; i < n_repeats; i++)
+    {
+        ofs << execution_time[i] << " ";
+    }
+    ofs << std::endl;
+
     return 0;
 }
