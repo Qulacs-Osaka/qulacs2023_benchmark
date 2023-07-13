57986317e36f17eb02e385ff970710a8e3ddbcc9
Glacialte <pikutaro25@gmail.com>
2023-07-13 16:16:20 +0900

    fix build.sh and build_by_docker.sh

--- benchmarks/Qulacs_now/build_by_docker.sh
+++ benchmarks/Qulacs_now/build_by_docker.sh
@@ -6,5 +6,3 @@ if [ ! -d ./qulacs ]; then
     USE_GPU=Yes pip install .
     sh /home/qulacs/script/build_gcc_with_gpu.sh
 fi
-
-nvcc -O2 -I ./qulacs/include -L ./qulacs/lib /home/main.cu -lvqcsim_static -lcppsim_static -lcsim_static -lgpusim_static -D _USE_GPU -lcublas -Xcompiler -fopenmp -o /home/main
