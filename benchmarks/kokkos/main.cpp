18d11b255e5baa1eac382a244e5a5b2558f3fc65
gandalfr-KY <90887153+gandalfr-KY@users.noreply.github.com>
2023-09-07 16:12:45 +0900

    Update benchmarks/kokkos/main.cpp
    target 重複の修正
    
    Co-authored-by: KowerKoint <50831431+KowerKoint@users.noreply.github.com>

--- benchmarks/kokkos/main.cpp
+++ benchmarks/kokkos/main.cpp
@@ -541,7 +541,10 @@ double double_control_matrix_bench(UINT n_qubits) {
         control_list(i, 1) = Kokkos::rand<decltype(random_generator), UINT>::draw(random_generator, 0, n_qubits - 2);
         if(targets(i, 0) == control_list(i, 0)) control_list(i, 0) = n_qubits - 1;
         if(targets(i, 0) == control_list(i, 1)) control_list(i, 1) = n_qubits - 2;
-        if(control_list(i, 0) == control_list(i, 1)) control_list(i, 1) = n_qubits - 1;
+        if(control_list(i, 0) == control_list(i, 1)) {
+            if(n_qubits - 1 == targets(i, 0)) control_lists(i, 1) = n_qubits - 2;
+            else control_list(i, 1) = n_qubits - 1;
+        }
         control_values(i, 0) = Kokkos::rand<decltype(random_generator), UINT>::draw(random_generator, 0, 1);
         control_values(i, 1) = Kokkos::rand<decltype(random_generator), UINT>::draw(random_generator, 0, 1);
         random_pool.free_state(random_generator);
